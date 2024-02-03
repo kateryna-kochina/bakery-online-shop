cc690f687ce6954a3e137994c4058649b0871e96
kateryna.kochina <kochina.ev@gmail.com>
2024-02-03 17:58:58 -0500

    Add navigation

--- general/general.py
+++ general/general.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template
 
 
-general_bp = Blueprint('general', __name__, template_folder='templates/general')
+general_bp = Blueprint('general', __name__, template_folder='templates')
 
 
 @general_bp.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('general/home.html')
 
 
 @general_bp.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('general/about.html')
