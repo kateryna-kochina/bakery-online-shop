c0b9f518eb115e8c06932188469ec54f9d207bad
kateryna.kochina <kochina.ev@gmail.com>
2024-02-15 19:52:44 -0500

    Improve select option section

--- app/products/forms.py
+++ app/products/forms.py
@@ -1,29 +1,20 @@
+###################################### EXAMPLE ######################################
+
 # from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField
+# from wtforms import IntegerField, SubmitField
 # from wtforms.validators import DataRequired
 # import requests
 
 
 # class RateMovieForm(FlaskForm):
-#     rating = StringField("Your Rating Out of 10 e.g. 7.5")
-#     review = StringField("Your Review")
+#     rating = IntegerField("Your Rating Out of 10 e.g. 7.5")
+#     review = IntegerField("Your Review")
 #     submit = SubmitField("Done")
 
 
 # class FindMovieForm(FlaskForm):
 #     title = StringField("Movie Title", validators=[DataRequired()])
 #     submit = SubmitField("Add Movie")
-
-
-# from flask import Flask, render_template, redirect, url_for, request
-
-# # routes
-# @app.route("/")
-# def home():
-#     result = db.session.execute(db.select(Movie))
-#     all_movies = result.scalars()
-#     return render_template("index.html", movies=all_movies)
-
 
 # @app.route("/edit", methods=["GET", "POST"])
 # def rate_movie():
@@ -47,3 +38,5 @@
 #         data = response.json()["results"]
 #         return render_template("select.html", options=data)
 #     return render_template("add.html", form=form)
+
+###################################### EXAMPLE ######################################
