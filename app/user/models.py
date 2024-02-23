5235ab345b7ad24bbbbf19ce5f687d8ac602efa2
kateryna.kochina <kochina.ev@gmail.com>
2024-02-23 11:28:43 -0500

    Update login manager, and use user authentication to make logout button visible

--- app/user/models.py
+++ app/user/models.py
@@ -3,9 +3,15 @@ from sqlalchemy import Column, Integer, String
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from ..database import db
+from ..login_manager import login_manager
 
 
-class User(UserMixin, db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
