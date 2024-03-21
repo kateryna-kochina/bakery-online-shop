2ca38446b051f4d7deb6d53a007c2ed2cd5fdb44
kateryna.kochina <kochina.ev@gmail.com>
2024-03-21 11:01:19 -0400

    Add login for removing items from the cart

--- app/cart/forms.py
+++ app/cart/forms.py
@@ -9,4 +9,9 @@ class UpdateQuantity(FlaskForm):
     quantity = IntegerField(
         'Quantity', validators=[DataRequired(), NumberRange(min=1), InputRequired()])
     update_btn = SubmitField('Update Quantity')
+    
+
+class RemoveCartItem(FlaskForm):
+    cart_item_id = HiddenField(
+        'Cart Item ID', id='cart_item', name='cart_item', validators=[DataRequired()])
     remove_btn = SubmitField('Remove Item From Cart')
--- app/cart/routes.py
+++ app/cart/routes.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_required
 
 from ..database import db
 from ..products.models import Option, Product
-from .forms import UpdateQuantity
+from .forms import UpdateQuantity, RemoveCartItem
 from .models import Cart, CartItem
 
 
@@ -19,6 +19,7 @@ def view_cart():
     if user_cart:
         cart_items = []
         forms = []
+        remove_item_forms = []
 
         for item in user_cart.items:
             print(item.id)
@@ -26,6 +27,10 @@ def view_cart():
             form = UpdateQuantity(prefix=str(item.id))
             form.cart_item_id.data = item.id
             forms.append(form)
+
+            remove_item_form = RemoveCartItem(prefix=str(item.id))
+            remove_item_form.cart_item_id.data = item.id
+            remove_item_forms.append(remove_item_form)
 
             product = Product.query.get(item.product_id)
             option = Option.query.get(item.option_id)
@@ -41,31 +46,32 @@ def view_cart():
             })
 
         if request.method == 'POST':
+            quantity_updated = False
+
+            # Check if any quantity update form was submitted
             for form in forms:
                 if form.validate_on_submit():
                     item_id = form.cart_item_id.data
                     new_quantity = form.quantity.data
-                    
-                    # # Update the quantity of the cart item
                     user_cart.update_quantity(item_id, new_quantity)
+                    quantity_updated = True
 
                     flash('Quantity updated successfully!', 'success')
-                    
-                    # if 'update_btn' in request.form:
-                    #     new_quantity = form.quantity.data
-                    #     user_cart.update_quantity(item_id, new_quantity)
 
-                    #     flash('Quantity updated successfully!', 'success')
+            # Process removal forms only if no quantity update form was submitted
+            if not quantity_updated:
+                for remove_item_form in remove_item_forms:
+                    if remove_item_form.validate_on_submit():
+                        item_id = remove_item_form.cart_item_id.data
+                        user_cart.remove_from_cart(item_id)
 
-                    # elif 'remove_btn' in request.form:
-                    #     user_cart.remove_from_cart(item_id)
+                        flash('Item deleted successfully!', 'success')
 
-                    #     flash('Item deleted successfully!', 'success')
-
-                    return redirect(url_for('cart.view_cart'))
+            return redirect(url_for('cart.view_cart'))
 
         return render_template('cart/cart.html',
                                forms=forms,
+                               remove_item_forms=remove_item_forms,
                                cart_items=cart_items)
 
     # If user's cart is empty, return an empty cart
